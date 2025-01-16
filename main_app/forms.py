078d4afabb608f5849e544a4a96dc9616a7f842f
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2025-01-16 13:38:40 +0300

    minor fixes

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,9 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile, Class, Attendance
 
-
 class UserRegistrationForm(forms.ModelForm):
-    password =forms.CharField(label='password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
@@ -14,16 +13,16 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Password don\'t match.')
-        return cd ['password2']
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio', 'Profile_img']
+        fields = ['bio', 'profile_img']
 
-class ClassForm(forms.ModelForm):
-    class Meta:
+class ClassForm(forms.ModelForm): 
+    class Meta: 
         model = Class
         fields = ['name', 'description']
 
