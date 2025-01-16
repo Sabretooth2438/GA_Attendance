6d34c5d7044b46f6b29a6492e330e9b232a4177a
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2025-01-16 10:54:50 +0300

    views.py modification

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserRegistrationForm, ProfileForm, ClassForm, StudentSearchForm, AttendanceForm
+from .models import Profile, Class, Attendance
+from django.urls import reverse_lazy
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save(commit=False)
+            user.set_password(user_form.cleaned_data['password'])
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.email = user.email
+            profile.save()
+            login(request, user)
+            return redirect('profile')  # Redirect to profile page after signup
+    else:
+        user_form = UserRegistrationForm()
+        profile_form = ProfileForm()
+    return render(request, 'registration/signup.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
