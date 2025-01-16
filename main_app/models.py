9ac25aa5221718074817c2feda7defc9027ce19e
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2025-01-16 10:50:23 +0300

    models.py modification

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('P', 'Present'),
+    ('A', 'Absent'),
+    ('L', 'Late'),
+]
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images/', blank=True, default="")
+    role = models.CharField(
+        max_length=10,
+        choices=[('Teacher', 'Teacher'), ('Student', 'Student')],
+        default='Student'
+    )
+
+    def __str__(self):
+        return self.user.username
